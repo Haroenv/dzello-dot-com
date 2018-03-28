e523e49897c0194da6c6d0be9490c28a47fbeafd
dzello <jdzielak@gmail.com>
2018-03-28 23:40:41 +0200

    Only initialize Algolia search if appId is provided

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,59 @@
-var instantsearch = window.instantsearch;
+function initializeSearch() {
 
-const search = instantsearch({
-  appId: window.algolia.appId,
-  apiKey: window.algolia.searchApiKey,
-  indexName: window.algolia.indexName,
-  searchParameters: {
-    hitsPerPage: 5,
-    filters: "kind:page AND draft:false"
-  },
-  searchFunction: function(helper) {
-    if (helper.state.query) {
-      helper.search();
-      $("#main-inner").addClass("search-active");
-      $("#algolia-attribution").addClass("search-active");
-    } else {
-      document.getElementById("algolia-hits").innerHTML = "";
-      $("#main-inner").removeClass("search-active");
-      $("#algolia-attribution").removeClass("search-active");
+  var instantsearch = window.instantsearch;
+
+  const search = instantsearch({
+    appId: window.algolia.appId,
+    apiKey: window.algolia.searchApiKey,
+    indexName: window.algolia.indexName,
+    searchParameters: {
+      hitsPerPage: 5,
+      filters: "kind:page AND draft:false"
+    },
+    searchFunction: function(helper) {
+      if (helper.state.query) {
+        helper.search();
+        $("#main-inner").addClass("search-active");
+        $("#algolia-attribution").addClass("search-active");
+      } else {
+        document.getElementById("algolia-hits").innerHTML = "";
+        $("#main-inner").removeClass("search-active");
+        $("#algolia-attribution").removeClass("search-active");
+      }
     }
-  }
-});
+  });
 
-search.addWidget(
-  instantsearch.widgets.hits({
-    container: '#algolia-hits',
-    templates: {
-      empty: '<p>N😭 RESULTS</p>',
-      item: `
-      <div>
-        <p>
-          <a href="{{{ url }}}">{{{_highlightResult.title.value}}}</a> &middot; {{{_snippetResult.content.value}}}
-        </p>
-      </div>
-      `,
-      footer: '<a href="https://algolia.com/"><img src="/images/search-by-algolia.svg"></a>'
-    }
-  })
-);
+  search.addWidget(
+    instantsearch.widgets.hits({
+      container: '#algolia-hits',
+      templates: {
+        empty: '<p>N😭 RESULTS</p>',
+        item: `
+        <div>
+          <p>
+            <a href="{{{ url }}}">{{{_highlightResult.title.value}}}</a> &middot; {{{_snippetResult.content.value}}}
+          </p>
+        </div>
+        `,
+        footer: '<a href="https://algolia.com/"><img src="/images/search-by-algolia.svg"></a>'
+      }
+    })
+  );
 
-search.addWidget(
-  instantsearch.widgets.searchBox({
-    container: '#algolia-search',
-    magnifier: false,
-    reset: false,
-    wrapInput: false,
-    autofocus: false
-  })
-);
+  search.addWidget(
+    instantsearch.widgets.searchBox({
+      container: '#algolia-search',
+      magnifier: false,
+      reset: false,
+      wrapInput: false,
+      autofocus: false
+    })
+  );
 
-search.start();
+  search.start();
+
+}
+
+if (window.algolia.appId) {
+  initializeSearch();
+}
