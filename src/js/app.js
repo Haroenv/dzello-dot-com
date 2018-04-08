a14c6c268ac0e8c96cc9cb8bb698407f892aff79
dzello <jdzielak@gmail.com>
2018-04-08 10:33:14 +0200

    Fix whitespace for nicely formatted HTML

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import search from "./search";
 import subscribe from "./subscribe";
 import discuss from "./discuss";
 import ascii from "./ascii";
+import twitter from "./twitter";
 
 // search();
 // subscribe();
 // discuss();
 // ascii();
+twitter();
 
 $(window).load(function() {
   setTimeout(function() {
