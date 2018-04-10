d431842d6bbb20612d37e6b47a0f1c6f7129c36a
dzello <jdzielak@gmail.com>
2018-04-11 01:10:33 +0200

    Move code that stops glitchy animation

--- src/js/app.js
+++ src/js/app.js
@@ -9,11 +9,5 @@ import ascii from "./ascii";
 // discuss();
 ascii();
 
-$(window).load(function() {
-  setTimeout(function() {
-    $(".h-title").removeClass("glitch-effect");
-  }, 2500);
-});
-
 var cl = cloudinary.Cloudinary.new( { cloud_name: "dzello"});
 cl.responsive();
